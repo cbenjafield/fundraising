425b7902630eec7d860257812347f399a1436cbb
Charlie Benjafield <me@cbenjafield.com>
2020-01-29 00:40:23 +0000

    Add intial database migrations

---
+++ database/migrations/2020_01_29_002751_create_donations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('campaign_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('is_anonymous');
+            $table->unsignedBigInteger('billable_id');
+            $table->decimal('amount', 11, 2);
+            $table->text('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
---
+++ database/migrations/2020_01_29_002803_create_assets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('original_filename');
+            $table->string('new_filename');
+            $table->string('path')->nullable();
+            $table->text('meta')->nullable();
+            $table->string('disk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
